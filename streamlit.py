146e0d27d0d28099cda0b40bd95890dc93fd26f7
SHRUTHI K <30868105+shruthi63@users.noreply.github.com>
2023-03-04 15:39:49 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -17,11 +17,11 @@ plan_option_range = (0, 10000)
 
 # Define the default values for the input fields
 default_values = {
-    'Vendor ID': '1834049',
-    'Client Key': '1834049',
-    'Appointment Number': '2758983',
-    'Plan Option 1 Deductible': 5000,
-    'Plan Option 1 Maximum Out of Pocket': 10000
+    'vendor_id': '1834049',
+    'client_key': '1834049',
+    'appointment_number': '2758983',
+    'plan_option1_deductible': 5000,
+    'plan_option1_maximum_out_of_pocket': 10000
 }
 
 # Create a function to get user input values as a DataFrame
@@ -29,9 +29,9 @@ def get_input_df():
     input_dict = {}
     for field in input_fields:
         if field.startswith('plan_option'):
-            input_dict[field] = st.slider(field, *plan_option_range, value=default_values[field])
+            input_dict[field] = st.slider(field.replace('_', ' ').title(), *plan_option_range, value=default_values[field])
         else:
-            input_dict[field] = st.text_input(field, default_values[field])
+            input_dict[field] = st.text_input(field.replace('_', ' ').title(), default_values[field])
     return pd.DataFrame([input_dict])
 
 # Create the Streamlit app
@@ -44,11 +44,11 @@ if st.button('Submit'):
     # If no input was provided, use the provided test example
     if input_df.empty:
         input_dict = {
-            'Vendor ID': '29177',
-            'Client Key': '1834049',
-            'Appointment Number': '2758983',
-            'Plan Option 1 Deductible': 3224876,
-            'Plan Option 1 Maximum Out of Pocket': 0.0
+            'vendor_id': '29177',
+            'client_key': '1834049',
+            'appointment_number': '2758983',
+            'plan_option1_deductible': 3224876,
+            'plan_option1_maximum_out_of_pocket': 0.0
         }
         input_df = pd.DataFrame([input_dict])
 
