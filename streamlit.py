80c1e43ff87a502a1ab21242314562feab2bdfa1
SHRUTHI K <30868105+shruthi63@users.noreply.github.com>
2023-03-04 13:11:07 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -24,26 +24,35 @@ default_values = {
     'plan_option1_maximum_out_of_pocket': 10000
 }
 
-# Create a function to get user input values as a numpy array
-def get_input_array():
+# Create a function to get user input values as a DataFrame
+def get_input_df():
     input_dict = {}
     for field in input_fields:
         if field.startswith('plan_option'):
             input_dict[field] = st.slider(field, *plan_option_range, value=default_values[field])
         else:
             input_dict[field] = st.text_input(field, default_values[field])
-    return np.array([list(input_dict.values())])
+    return pd.DataFrame([input_dict])
 
 # Create the Streamlit app
 st.title('Cancellation Flag Predictor')
 
 # Get user input values
-input_array = get_input_array()
+input_df = get_input_df()
 
-# Create a submit button to trigger the prediction
-if st.button('Predict'):
-    # Make predictions using the machine learning model
-    prediction = model.predict(input_array)
+# If no input was provided, use the provided test example
+if input_df.empty:
+    input_dict = {
+        'vendor_id': '29177',
+        'client_key': '1834049',
+        'appointment_number': '2758983',
+        'plan_option1_deductible': 3224876,
+        'plan_option1_maximum_out_of_pocket': 0.0
+    }
+    input_df = pd.DataFrame([input_dict])
 
-    # Display the predicted cancellation flag value
-    st.write('Predicted Cancellation Flag:', prediction[0])
+# Make predictions using the machine learning model
+prediction = model.predict(input_df)
+
+# Display the predicted cancellation flag value
+st.write('Predicted Cancellation Flag:', prediction[0])
