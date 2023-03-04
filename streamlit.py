1e0b92c7f14ad8607a6137971937a4a2a98aad76
SHRUTHI K <30868105+shruthi63@users.noreply.github.com>
2023-03-04 13:05:05 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,7 @@ import urllib.request
 import streamlit as st
 import pandas as pd
 import joblib
+import numpy as np
 
 url = 'https://github.com/shruthi63/curly-pancake/blob/main/model.joblib?raw=true'
 filename = 'model.joblib'
@@ -23,24 +24,26 @@ default_values = {
     'plan_option1_maximum_out_of_pocket': 10000
 }
 
-# Create a function to get user input values as a DataFrame
-def get_input_df():
+# Create a function to get user input values as a numpy array
+def get_input_array():
     input_dict = {}
     for field in input_fields:
         if field.startswith('plan_option'):
             input_dict[field] = st.slider(field, *plan_option_range, value=default_values[field])
         else:
             input_dict[field] = st.text_input(field, default_values[field])
-    return pd.DataFrame([input_dict])
+    return np.array([list(input_dict.values())])
 
 # Create the Streamlit app
 st.title('Cancellation Flag Predictor')
 
 # Get user input values
-input_df = get_input_df()
+input_array = get_input_array()
 
-# Make predictions using the machine learning model
-prediction = model.predict(input_df)
+# Create a submit button to trigger the prediction
+if st.button('Predict'):
+    # Make predictions using the machine learning model
+    prediction = model.predict(input_array)
 
-# Display the predicted cancellation flag value
-st.write('Predicted Cancellation Flag:', prediction[0])
+    # Display the predicted cancellation flag value
+    st.write('Predicted Cancellation Flag:', prediction[0])
