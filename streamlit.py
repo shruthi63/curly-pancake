efa0acd08f660a1ed36936a99be32719f5fb4cee
SHRUTHI K <30868105+shruthi63@users.noreply.github.com>
2023-03-04 13:16:54 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,7 @@ import urllib.request
 import streamlit as st
 import pandas as pd
 import joblib
+import numpy as np
 
 url = 'https://github.com/shruthi63/curly-pancake/blob/main/model.joblib?raw=true'
 filename = 'model.joblib'
@@ -28,24 +29,30 @@ def get_input_df():
     input_dict = {}
     for field in input_fields:
         if field.startswith('plan_option'):
-            input_dict[field] = st.slider(f"{field} ({default_values[field]})", *plan_option_range, value=default_values[field])
+            input_dict[field] = st.slider(field, *plan_option_range, value=default_values[field])
         else:
             input_dict[field] = st.text_input(field, default_values[field])
-    input_df = pd.DataFrame([input_dict])
-    return input_df
+    return pd.DataFrame([input_dict])
 
 # Create the Streamlit app
 st.title('Cancellation Flag Predictor')
 
 # Get user input values
-if st.button('Predict'):
-    input_df = get_input_df()
+input_df = get_input_df()
 
-    # Make predictions using the machine learning model
-    prediction = model.predict(input_df)
+# If no input was provided, use the provided test example
+if input_df.empty:
+    input_dict = {
+        'vendor_id': '29177',
+        'client_key': '1834049',
+        'appointment_number': '2758983',
+        'plan_option1_deductible': 3224876,
+        'plan_option1_maximum_out_of_pocket': 0.0
+    }
+    input_df = pd.DataFrame([input_dict])
 
-    # Display the predicted cancellation flag value
-    st.write('Predicted Cancellation Flag:', prediction[0])
+# Make predictions using the machine learning model
+prediction = model.predict(input_df)
 
-# Add a test example to prefill the input fields
-st.write('Test Example:', 'vendor_id=29177, client_key=1834049, appointment_number=2758983, plan_option1_deductible=3224876, plan_option1_maximum_out_of_pocket=0.0, 1500.0')
+# Display the predicted cancellation flag value
+st.write('Predicted Cancellation Flag:', prediction[0])
