4847fb54b632070429e11fcd479ab7601353131d
SHRUTHI K <30868105+shruthi63@users.noreply.github.com>
2023-03-05 21:19:28 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -51,6 +51,9 @@ def get_input_df():
 # Create the Streamlit app
 st.title('Cancellation Flag Predictor')
 
+# Display the input fields
+input_df = get_input_df()
+
 # Get user input values when submit button is clicked
 if st.button('Submit'):
     try:
