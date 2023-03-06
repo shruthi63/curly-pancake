1e0db6ad9c9d5252e89cc6e18a84c6e8db69a334
SHRUTHI K <30868105+shruthi63@users.noreply.github.com>
2023-03-05 22:07:45 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -18,15 +18,14 @@ except:
     st.stop()
 
 # Define the input fields
-input_fields = ['client_key', 'auth_status', 'member_health_plan_id', 'primary_cpt', 'vendor_id', 'plan_option1_coinsurance_member',
-                'appointment_number', 'plan_option1_deductible', 'plan_option1_maximum_out_of_pocket']
+input_fields = ['auth_status', 'member_health_plan_id', 'primary_cpt', 'vendor_id', 'plan_option1_coinsurance_member',
+                'appointment_number', 'client_key','plan_option1_deductible', 'plan_option1_maximum_out_of_pocket']
 
 # Define the range for the plan option input fields
 plan_option_range = (0, 10000)
 
 # Define the default values for the input fields
 default_values = {
-    'client_key': '',
     'auth_status': '',
     'member_health_plan_id': '',
     'primary_cpt': '',
@@ -34,7 +33,8 @@ default_values = {
     'plan_option1_coinsurance_member': 20,
     'appointment_number': '',
     'plan_option1_deductible': 5000,
-    'plan_option1_maximum_out_of_pocket': 10000
+    'plan_option1_maximum_out_of_pocket': 10000,
+    'client_key':''
 }
 
 # Create a function to get user input values as a DataFrame
@@ -51,22 +51,18 @@ def get_input_df():
 
 # Create the Streamlit app
 st.title('Cancellation Flag Predictor')
+
+# Display the input fields
 input_df = get_input_df()
-# Show the input fields
-for field in input_fields:
-    if field == 'auth_status':
-        st.selectbox(field, ['Claim Received', 'Approved', 'Cancelled', 'Submitted For Cancellation', 'Progyny Cover Cost'], index=0)
-    else:
-        st.text_input(field, default_values[field])
+
 # Get user input values when submit button is clicked
 if st.button('Submit'):
-   
+    
         input_df = get_input_df()
 
         # If no input was provided, use the provided test example
         if input_df.empty:
             input_dict = {
-                'client_key': '',
                 'auth_status': 'Claim Received',
                 'member_health_plan_id': 'HP000001',
                 'primary_cpt': 'A123',
@@ -74,10 +70,9 @@ if st.button('Submit'):
                 'appointment_number': '2758983',
                 'plan_option1_coinsurance_member': 20,
                 'plan_option1_deductible': 3224876,
-                'plan_option1_maximum_out_of_pocket': 0.0
+                'plan_option1_maximum_out_of_pocket': 0.0,
+                'client_key':'A23'
             }
-           
-
             input_df = pd.DataFrame([input_dict])
 
         # Make predictions using the machine learning model
@@ -85,3 +80,4 @@ if st.button('Submit'):
 
         # Display the predicted cancellation flag value
         st.write('Predicted Cancellation Flag:', prediction[0])
+    
