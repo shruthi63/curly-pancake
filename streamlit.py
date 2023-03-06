9e6c133d97a2a54f8c6ff213a59fae0c7699da58
SHRUTHI K <30868105+shruthi63@users.noreply.github.com>
2023-03-05 23:16:35 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -9,17 +9,12 @@ from sklearn.metrics import f1_score
 
 url = 'https://github.com/shruthi63/curly-pancake/blob/main/model2.joblib?raw=true'
 filename = 'model2.joblib'
-
-try:
-    urllib.request.urlretrieve(url, filename)
-    model2 = joblib.load(filename)
-except:
-    st.error("Failed to download model. Please check the URL or try again later.")
-    st.stop()
+urllib.request.urlretrieve(url, filename)
+model = joblib.load(filename)
 
 # Define the input fields
 input_fields = ['auth_status', 'member_health_plan_id', 'primary_cpt', 'vendor_id', 'plan_option1_coinsurance_member',
-                'appointment_number', 'client_key','plan_option1_deductible', 'plan_option1_maximum_out_of_pocket']
+                'appointment_number', 'plan_option1_deductible', 'plan_option1_maximum_out_of_pocket']
 
 # Define the range for the plan option input fields
 plan_option_range = (0, 10000)
@@ -33,8 +28,7 @@ default_values = {
     'plan_option1_coinsurance_member': 20,
     'appointment_number': '',
     'plan_option1_deductible': 5000,
-    'plan_option1_maximum_out_of_pocket': 10000,
-    'client_key':''
+    'plan_option1_maximum_out_of_pocket': 10000
 }
 
 # Create a function to get user input values as a DataFrame
@@ -44,37 +38,48 @@ def get_input_df():
         if field.startswith('plan_option'):
             input_dict[field] = st.slider(field, *plan_option_range, value=default_values[field])
         elif field == 'auth_status':
-            input_dict[field] = st.selectbox(field, ['Claim Received', 'Approved', 'Cancelled', 'Submitted For Cancellation', 'Progyny Cover Cost'], index=0)
+            auth_status = st.selectbox(field, ['Claim Received', 'Approved', 'Cancelled', 'Submitted For Cancellation', 'Progyny Cover Cost'], index=0)
+            # Map the auth_status value to a numerical value
+            if auth_status == 'Cancelled':
+                input_dict[field] = 1
+            elif auth_status == 'Approved':
+                input_dict[field] = 0
+            elif auth_status == 'Claim Received':
+                input_dict[field] = 2
+            elif auth_status == 'Submitted For Cancellation':
+                input_dict[field] = 3
+            else:
+                input_dict[field] = ''
         else:
             input_dict[field] = st.text_input(field, default_values[field])
     return pd.DataFrame([input_dict])
 
 # Create the Streamlit app
 st.title('Cancellation Flag Predictor')
-
-# Display the input fields
 input_df = get_input_df()
-
 # Get user input values when submit button is clicked
 if st.button('Submit'):
+    try:
+        input_df = get_input_df()
 
-    # If no input was provided, use the provided test example
-    if input_df.empty:
-        input_dict = {
-            'auth_status': 'Claim Received',
-            'member_health_plan_id': 'HP000001',
-            'primary_cpt': 'A123',
-            'vendor_id': '29177',
-            'appointment_number': '2758983',
-            'plan_option1_coinsurance_member': 20,
-            'plan_option1_deductible': 3224876,
-            'plan_option1_maximum_out_of_pocket': 0.0,
-            'client_key':'A23'
-        }
-        input_df = pd.DataFrame([input_dict])
+        # If no input was provided, use the provided test example
+        if input_df.empty:
+            input_dict = {
+                'auth_status': 'Claim Received',
+                'member_health_plan_id': 'HP000001',
+                'primary_cpt': 'A123',
+                'vendor_id': '29177',
+                'appointment_number': '2758983',
+                'plan_option1_coinsurance_member': 20,
+                'plan_option1_deductible': 3224876,
+                'plan_option1_maximum_out_of_pocket': 0.0
+            }
+            input_df = pd.DataFrame([input_dict])
 
-    # Make predictions using the machine learning model
-    prediction = model2.predict(input_df)
+        # Make predictions using the machine learning model
+        prediction = model.predict(input_df)
 
-    # Display the predicted cancellation flag value
-    st.write('Predicted Cancellation Flag:', prediction[0])
+        # Display the predicted cancellation flag value
+        st.write('Predicted Cancellation Flag:', prediction[0])
+    except:
+        st.write('Error: Please enter valid input values.')
