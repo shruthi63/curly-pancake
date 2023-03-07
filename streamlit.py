2663a00e402eaf82ac002697d39983aa4e13cdf0
SHRUTHI K <30868105+shruthi63@users.noreply.github.com>
2023-03-06 20:02:53 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -28,19 +28,18 @@ default_values = {
 # Create a function to get user input values as a DataFrame
 def get_input_df():
     input_dict = {}
-    with st.form(key='input_form'):
-        for field in input_fields:
-            if field.startswith('plan_option'):
-                col1, col2 = st.beta_columns(2)
-                with col1:
-                    st.write(field.replace('_', ' ').title())
-                with col2:
-                    input_value = st.text_input('', default_values[field])
-                    slider_value = st.slider('', *plan_option_range, value=int(input_value))
-                    input_dict[field] = slider_value
-            else:
-                input_dict[field] = st.text_input(field.replace('_', ' ').title(), default_values[field])
-        submit_button = st.form_submit_button(label='Submit')
+    for field in input_fields:
+        if field.startswith('plan_option'):
+            # Add slider and text box for plan option input fields
+            col1, col2 = st.beta_columns(2)
+            with col1:
+                st.write(field.replace('_', ' ').title())
+            with col2:
+                input_value = st.text_input('', default_values[field])
+                slider_value = st.slider('', *plan_option_range, value=int(input_value))
+                input_dict[field] = slider_value
+        else:
+            input_dict[field] = st.text_input(field.replace('_', ' ').title(), default_values[field])
     return pd.DataFrame([input_dict])
 
 # Create the Streamlit app
@@ -50,10 +49,21 @@ st.title('Claim Predictor')
 input_df = get_input_df()
 
 # Get user input values when submit button is clicked
-if st.button('Submit'):
+if not input_df.empty:
     # Make predictions using the machine learning model
     prediction = model.predict(input_df)
-    if prediction[0] == 0:
+    if(prediction[0]==0):
+        # Display the predicted cancellation flag value
         st.write('Predicted Cancellation Flag:', 'Authorization/Appointment will be claimed')
     else:
         st.write('Predicted Cancellation Flag:', 'Likely to get cancelled')
+    
+    # Show the submit button only if user has entered any input values
+    if st.button('Submit'):
+        # Make predictions using the machine learning model
+        prediction = model.predict(input_df)
+        if(prediction[0]==0):
+            # Display the predicted cancellation flag value
+            st.write('Predicted Cancellation Flag:', 'Authorization/Appointment will be claimed')
+        else:
+            st.write('Predicted Cancellation Flag:', 'Likely to get cancelled')
